07fee043aeaedc8a0366daa82d2a356cec57d9e8
uselessbubbles <sim.jimmichan@gmail.com>
2016-07-07 18:29:26 +0100

    Add files via upload

--- bubbles.js
+++ bubbles.js
@@ -156,8 +156,8 @@ function initEventListeners() {
 
 function updateCanvasDimensions() {
     canvas.attr({
-        height: 500,
-        width: 1000
+        height: 600,
+        width: 1050
     });
     canvasWidth = canvas.width();
     canvasHeight = canvas.height();
